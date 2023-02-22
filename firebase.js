000ece1bbbeb874b894a4e8c869a167498c8c856
iAdityarajpurohit <108542528+iAdityarajpurohit@users.noreply.github.com>
2023-02-22 22:42:53 +0530

    message

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAfK2Uve6WxTTrWa0jBbeaaGoQoi9yKP-8",
+    authDomain: "fir-projct-8f9b0.firebaseapp.com",
+    projectId: "fir-projct-8f9b0",
+    storageBucket: "fir-projct-8f9b0.appspot.com",
+    messagingSenderId: "794842241885",
+    appId: "1:794842241885:web:e7ae56703dd6f2a0b9e6b4",
+    measurementId: "G-3DWSF7Y2CL"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
